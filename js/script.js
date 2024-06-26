3f8ee57219359ed9be01782f78295b61dac81ec9
andreacagnina <amirkal@libero.it>
2024-06-26 17:11:18 +0200

    [FEAT] create le condizioni in js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,20 @@ btn.addEventListener('click', function () {
     console.log(km)
     let age = document.getElementById('age').value;
     console.log(age)
-    let price = 0.21 * km
+    let price = 0.21 * km;
     console.log(price)
+
+    if (age == 0) {
+        let discount = price * 0.2;
+        price -= discount;
+        console.log(price);
+
+    }
+    else if (age == 2) {
+        let discount = price * 0.4;
+        price -= discount;
+        console.log(price);
+    }
+
+
 })

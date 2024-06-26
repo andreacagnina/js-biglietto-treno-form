af18f20f136eeccb681b49862344e4f55fd560c2
andreacagnina <amirkal@libero.it>
2024-06-26 19:44:12 +0200

    [FEAT] ADDED NAME AND MOD DEAL

--- js/script.js
+++ js/script.js
@@ -1,29 +1,42 @@
 let btn = document.getElementById('btn');
 btn.addEventListener('click', function () {
 
+    let name = document.getElementById('name').value;
+    console.log(name)
+
+
     let km = document.getElementById('km').value;
-    console.log(km)
+
 
     let age = document.getElementById('age').value;
-    console.log(age)
+
 
     let price = 0.21 * km;
-    console.log(price)
+
 
     if (age == 1) {
         let discount = price * 0.2;
         price -= discount;
-        console.log(price);
+        document.getElementById('deal').innerText = `Offerta Young`;
+
 
     }
     else if (age == 3) {
         let discount = price * 0.4;
         price -= discount;
-        console.log(price);
+        document.getElementById('deal').innerText = `Offerta Senior`;
+
     }
 
+    else {
+        document.getElementById('deal').innerText = `Offerta Standard`;
+
+    }
+
+    document.getElementById('name_passenger').innerHTML = name;
     document.getElementById('message').innerHTML = price.toFixed(2) + ' ' + '&euro;';
     document.getElementById('vagon').innerHTML = Math.floor(Math.random() * 10) + 1;
     document.getElementById('cp').innerHTML = Math.floor(Math.random() * 100000) + 1;
+
 
 })

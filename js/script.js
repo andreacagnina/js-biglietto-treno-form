5713e225503e470858399567098c938965afea59
andreacagnina <amirkal@libero.it>
2024-06-26 17:39:50 +0200

    [fix] fixed form === to ==

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
-addEventListener('click', function () {
+let btn = document.getElementById('btn');
+btn.addEventListener('click', function () {
+
     let km = document.getElementById('km').value;
     console.log(km)
+
     let age = document.getElementById('age').value;
     console.log(age)
+
     let price = 0.21 * km;
     console.log(price)
 
-    if (age == 0) {
+    if (age == 1) {
         let discount = price * 0.2;
         price -= discount;
         console.log(price);
 
     }
-    else if (age == 2) {
+    else if (age == 3) {
         let discount = price * 0.4;
         price -= discount;
         console.log(price);
     }
 
-
+    document.getElementById('message').innerHTML = price.toFixed(2);
 })
